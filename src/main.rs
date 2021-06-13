40a5eaefdaa4e98068e26da320e4ac8eeb74194c
Parham Alvani <parham.alvani@gmail.com>
2021-06-13 23:34:30 +0430

    feat: Load Configuration with OS Arguments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 mod handler;
-mod settings;
 
+use clap::{App, Arg};
 use std::net::TcpListener;
 use std::thread;
 
 fn main() {
-    let s = settings::Settings::new().expect("loading configuration failed");
+    let matches = App::new("echo.rs says what you said")
+        .author("Parham Alvani <parham.alvani@gmail.com>")
+        .arg(
+            Arg::with_name("listen")
+                .short("l")
+                .takes_value(true)
+                .value_name("address")
+                .help("listen address e.g. 127.0.0.1:1378"),
+        )
+        .get_matches();
 
-    let listener =
-        TcpListener::bind(format!("{}:{}", s.host(), s.port())).expect("listening failed");
+    let listener = TcpListener::bind(matches.value_of("listen").unwrap_or("127.0.0.1:1378"))
+        .expect("listening failed");
 
     println!("Listening on {:?}", listener);
 
