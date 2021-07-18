cc2e165229daafd7dc0bd73ee131d9f7a0f36191
Parham Alvani <parham.alvani@gmail.com>
2021-07-18 08:45:39 +0430

    feat: Remove External Dependency

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 mod handler;
 
-use clap::{App, Arg};
 use std::net::TcpListener;
 use std::thread;
 
 fn main() {
-    let matches = App::new("echo.rs says what you said")
-        .author("Parham Alvani <parham.alvani@gmail.com>")
-        .arg(
-            Arg::with_name("listen")
-                .short("l")
-                .takes_value(true)
-                .value_name("address")
-                .help("listen address e.g. 127.0.0.1:1378"),
-        )
-        .get_matches();
-
-    let listener = TcpListener::bind(matches.value_of("listen").unwrap_or("127.0.0.1:1378"))
-        .expect("listening failed");
+    let listener = TcpListener::bind(
+        std::env::args()
+            .nth(1)
+            .unwrap_or_else(|| "127.0.0.1:1378".to_string()),
+    )
+    .expect("listening failed");
 
     println!("Listening on {:?}", listener);
 
